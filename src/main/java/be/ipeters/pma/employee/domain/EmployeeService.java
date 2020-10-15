add6d92af098e1318f1fefcc042b8d309e30d47e
cpjjpeters <cpjjpeters@gmail.com>
2020-10-15 14:09:15 +0200

    second try to add lombok and push to github

---
+++ src/main/java/be/ipeters/pma/employee/domain/EmployeeService.java
@@ -0,0 +1,10 @@
+package be.ipeters.pma.employee.domain;
+
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class EmployeeService {
+
+}
