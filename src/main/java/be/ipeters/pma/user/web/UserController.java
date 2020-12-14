74398171edd41757abcc7c5b17088a556404eb30
cpjjpeters <cpjjpeters@gmail.com>
2020-12-14 07:28:58 +0100

    usercontroller

--- src/main/java/be/ipeters/pma/user/web/UserController.java
+++ src/main/java/be/ipeters/pma/user/web/UserController.java
@@ -22,18 +22,19 @@ public class UserController {
 	}
 
 	@GetMapping("/{userId}/invoices")
-	public String displayUserInvoices(@PathVariable int userId, @RequestParam(value="date", required=false) Date date) {
-		return "invoice fo user found: " + userId + " on date: " +date; // MMddyyyy
+	public String displayUserInvoices(@PathVariable int userId,
+			@RequestParam(value = "date", required = false) Date date) {
+		return "invoice fo user found: " + userId + " on date: " + date; // MMddyyyy
 	}
 
 	@GetMapping("/{id}/items")
 	public List<String> displayStringJson(@PathVariable int id) {
-		return Arrays.asList("shoes","laptop","button");
+		return Arrays.asList("shoes", "laptop", "button");
 	}
-	
+
 	@GetMapping("/{id}/products_as_json")
 	public List<Product> displayProductsJson(@PathVariable int id) {
-		return Arrays.asList(new Product(1,"shoes",42.99),
-				new Product(2,"laptop",3090.00), new Product(3,"button", 05.55));
+		return Arrays.asList(new Product(1, "shoes", 42.99), new Product(2, "laptop", 3090.00),
+				new Product(3, "button", 05.55));
 	}
 }
